098fe1bbf84c580c469b8c5a2245eca3466c54cf
Garavirod <rodrigogarciaavila26@gmail.com>
2019-05-24 16:56:32 -0500

    Administrador de servicios

--- Backend/escommunity/services/models.py
+++ Backend/escommunity/services/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 class Service(models.Model):
     nameModule = models.CharField(max_length=200, verbose_name="Nombre del módulo")       
     nameAdmin = models.CharField(max_length=200, verbose_name="Nombre completo del admin") 
-    eMail = models.CharField(max_length=200, verbose_name="E-mail del admin") 
+    eMail = models.CharField(max_length=200, verbose_name="E-mail del administrador") 
     cel = models.CharField(max_length=10, verbose_name="Tel del admin") 
     descModule = models.TextField(verbose_name="Descripcion del módulo",blank=True) 
-    image = models.ImageField(verbose_name="Imagen", upload_to="servicios")
+    image = models.ImageField(verbose_name="Imagen", upload_to="services")
     created = models.DateTimeField(auto_now_add=True,verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True,verbose_name="Fecha de edición")
     user = models.ForeignKey(User,verbose_name="Administrador del módulo",on_delete=models.CASCADE)
